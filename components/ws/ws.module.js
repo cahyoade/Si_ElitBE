726d8a2ad0d2cb9cb067ce20f41b9acdf1abb287
Cahyo Ade <cahyoade8@gmail.com>
2024-05-25 02:43:36 +0700

    fix devicestatus and implement ping pong

--- components/ws/ws.module.js
+++ components/ws/ws.module.js
@@ -2,7 +2,6 @@ import AttendanceService from "../attendance/attendance.service.js";
 import AuthService from "../auth/auth.service.js";
 import { WebSocketServer } from "ws";
 import express from 'express';
-import e from "express";
 
 class WsModule {
 	constructor(env, server) {
@@ -10,18 +9,39 @@ class WsModule {
 		this.authService = new AuthService(env);
 		this.wss = new WebSocketServer({ server });
 		this.count = 0;
+		this.deviceStatus = env.devices.split(',').reduce((obj, key) => ({...obj, [key]: false}), {});
 
 		this.wss.on('connection', ws => {
 			if(!(env.devices.split(',').includes(ws.protocol))) ws.close();
 			ws.name = ws.protocol;
+			this.deviceStatus[ws.name] = true;
 			ws.on('message', payload => {
+					if (payload.toString() === 'pong') {
+						this.deviceStatus[ws.name] = true;
+						return;
+					}
+
 					const [type, data] = payload.toString().split(',');
 					if (data && type === 'req_att') {
 						this.handleAttendance(ws, data)
 					}
 			});
+			ws.on('error', console.error);
+		});
+
+		setInterval(this.ping, 10000);
+
+	}
+
+	ping = () => {
+		this.wss.clients.forEach(ws => {
+			this.deviceStatus[ws.name] = false
+			if (ws.readyState === 1) {
+				ws.send('ping');
+			}
 		});
 	}
+
 
 	create() {
 		return {router: this.getRouter()};
@@ -45,11 +65,11 @@ class WsModule {
       ws.send(`res_att,0,${err},${cardId}`);
     }
   }
-	
-	getDevicesStatus = () => {
-		return [...this.wss.clients].reduce((obj, key) => ({...obj, [key.name]: key.readyState === 1}), {})
-	}
 
+	getDevicesStatus = () => {
+		return this.deviceStatus;
+	}
 }
+
 
 export default WsModule;
